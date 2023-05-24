db04962044542721060fc3cd165df3f40dd084b1
yivlad <jacenko.vlad@gmail.com>
2023-05-24 16:56:07 +0200

    Use viem and latest wagmi

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,11 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { WagmiConfig } from 'wagmi'
 
-import { client } from '../lib/wagmi'
+import { config } from '../lib/wagmi'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <WagmiConfig client={client}>
+    <WagmiConfig config={config}>
       <Component {...pageProps} />
     </WagmiConfig>
   )
